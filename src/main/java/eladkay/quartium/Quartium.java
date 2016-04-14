943f2f2c38531ddb4ff5fed3d89d2749d68f5ed5
Elad Kinsbruner <eladkay@gmail.com>
2016-04-14 14:49:08 +0300

    Added actual stuff

---
+++ src/main/java/eladkay/quartium/Quartium.java
@@ -0,0 +1,7 @@
+package eladkay.quartium;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = References.MODID, name = References.NAME, version = References.VERSION, dependencies = References.DEPENDENCIES, acceptedMinecraftVersions = References.ACCEPTED_VERSION, guiFactory = References.GUI_FACTORY)
+public class Quartium {
+}
---
+++ src/main/java/eladkay/quartium/References.java
@@ -0,0 +1,10 @@
+package eladkay.quartium;
+
+public class References {
+    public static final String DEPENDENCIES = "";
+    public static final String VERSION = "alpha";
+    public static final String MODID = "Quartium";
+    public static final String NAME = "Quartium";
+    public static final String ACCEPTED_VERSION = "1.9";
+    public static final String GUI_FACTORY = "";
+}
