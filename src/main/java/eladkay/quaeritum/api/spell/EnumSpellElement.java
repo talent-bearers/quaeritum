b468b1e4f0547a426c2ad3be53718058a0f1c868
yrsegal <yrsegal@gmail.com>
2017-07-02 12:18:10 +0300

    change to using Color
    this gives the intellij color picker, as well as making it more clear
    what determines rainbow.

--- src/main/java/eladkay/quaeritum/api/spell/EnumSpellElement.java
+++ src/main/java/eladkay/quaeritum/api/spell/EnumSpellElement.java
@@ -3,6 +3,7 @@ package eladkay.quaeritum.api.spell;
 import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.awt.*;
 
 /**
@@ -10,21 +11,22 @@ import java.awt.*;
  *         Created at 11:20 PM on 7/1/17.
  */
 public enum EnumSpellElement {
-    EARTH(0x603D0E),
-    WATER(0x1325EF),
-    FIRE(0xF98B16),
-    AIR(0xEFED7F),
-    METAL(0x8E8E8E),
-    ENTROPY(0x631010),
-    FORM(0xA09F85),
-    CONNECTION(0x19C3E5),
-    SPIRIT(0x91820E),
-    AETHER(-1),
-    SOUL(0xA93AAD);
+    EARTH(new Color(0x794715)),
+    WATER(new Color(0x1A6BEF)),
+    FIRE(new Color(0xF98B16)),
+    AIR(new Color(0xEFED7F)),
+    METAL(new Color(0x8E8E8E)),
+    ENTROPY(new Color(0x7D0D0F)),
+    FORM(new Color(0x3DD748)),
+    CONNECTION(new Color(0x19C3E5)),
+    SPIRIT(new Color(0xB4AE28)),
+    AETHER(null),
+    SOUL(new Color(0xCF42D3));
 
-    private final int color; // -1 is treated specially, as rainbow.
+    @Nullable
+    private final Color color; // null is treated specially, as rainbow.
 
-    EnumSpellElement(int color) {
+    EnumSpellElement(@Nullable Color color) {
         this.color = color;
     }
 
@@ -34,7 +36,7 @@ public enum EnumSpellElement {
      * @return A color value from 0 to 0xffffff.
      */
     public int color(@Nonnull World world) {
-        if (color != -1) return color;
+        if (color != null) return color.getRGB();
         return Color.HSBtoRGB((world.getTotalWorldTime() * 2L % 360L) / 360.0F, 0.8F, 1.0F);
     }
 
