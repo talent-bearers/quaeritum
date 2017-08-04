da61c78d968fd45f79e0488a787394c5ed66be7c
saad <saadodi44@gmail.com>
2017-08-04 19:08:24 +0300

    rift done (i guess)

--- src/main/java/eladkay/quaeritum/client/core/RiftRenderer.java
+++ src/main/java/eladkay/quaeritum/client/core/RiftRenderer.java
@@ -1,6 +1,6 @@
 package eladkay.quaeritum.client.core;
 
-import eladkay.quaeritum.api.util.RandUtil;
+import com.teamwizardry.librarianlib.core.client.ClientTickHandler;
 import eladkay.quaeritum.api.util.RandUtilSeed;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
@@ -8,6 +8,7 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.common.MinecraftForge;
@@ -15,9 +16,13 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL14;
 
 import java.awt.*;
 import java.util.ArrayList;
+
+import static org.lwjgl.opengl.GL14.GL_FUNC_ADD;
+import static org.lwjgl.opengl.GL14.GL_FUNC_REVERSE_SUBTRACT;
 
 /**
  * Created by LordSaad.
@@ -27,19 +32,22 @@ public class RiftRenderer {
 	private final Vec3d center;
 	private final long seed;
 
-	private net.minecraft.client.renderer.VertexBuffer BUILDER = new net.minecraft.client.renderer.VertexBuffer(0xFFFF);
-	private ArrayList<Vec3d> points = new ArrayList<>();
 	private ArrayList<Vec3d> points1 = new ArrayList<>();
 	private ArrayList<Vec3d> points2 = new ArrayList<>();
+	private ArrayList<Vec3d> points3 = new ArrayList<>();
+	private ArrayList<Vec3d> points4 = new ArrayList<>();
 
-	private double DIST = 2.5;
-	private double RANDOM_VARIATION = 2; // end-to-end distance randomness
-	private double HEIGHT_VARIATION = 2; // Rift Tilt
+	private ArrayList<Vec3d> normalPoints1 = new ArrayList<>();
+	private ArrayList<Vec3d> normalPoints2 = new ArrayList<>();
+	private ArrayList<Vec3d> normalPoints3 = new ArrayList<>();
+	private ArrayList<Vec3d> normalPoints4 = new ArrayList<>();
+
 	private double JAGGED_RESOLUTION = 0.1;
-	private double SEGMENT_COUNT = 100;
-	private double HEIGHT = 3.5;
+	private double SEGMENT_COUNT = 5;
 
-	private boolean flip = true;
+	private boolean flip1 = true;
+	private boolean flip2 = true;
+	private boolean flip3 = true;
 
 	public RiftRenderer(Vec3d center, long seed) {
 		this.center = center;
@@ -47,41 +55,99 @@ public class RiftRenderer {
 		MinecraftForge.EVENT_BUS.register(this);
 
 		RandUtilSeed rand = new RandUtilSeed(seed);
+		ArrayList<Vec3d> points1 = new ArrayList<>();
+		ArrayList<Vec3d> points2 = new ArrayList<>();
 
-		Vec3d point1 = new Vec3d(
-				rand.nextDouble(DIST - RANDOM_VARIATION, DIST),
-				rand.nextDouble(-HEIGHT_VARIATION, HEIGHT_VARIATION),
-				rand.nextDouble(DIST - RANDOM_VARIATION, DIST));
+		points1.add(new Vec3d(0, 20, 0));
+		points1.add(new Vec3d(4, 10, 0));
+		points1.add(new Vec3d(0, 0, 0));
 
-		Vec3d point2 = new Vec3d(
-				rand.nextDouble(DIST, DIST + RANDOM_VARIATION),
-				rand.nextDouble(-HEIGHT_VARIATION, HEIGHT_VARIATION),
-				rand.nextDouble(DIST, DIST + RANDOM_VARIATION));
+		points2.add(new Vec3d(0, 20, 0));
+		points2.add(new Vec3d(-4, 10, 0));
+		points2.add(new Vec3d(0, 0, 0));
 
-		Vec3d diff = point1.subtract(point2);
+		for (int i = 0; i < points1.size() - 1; i++) {
+			Vec3d point = points1.get(i);
+			Vec3d secPoint = points1.get(i + 1);
 
-		double SEPARATION = diff.lengthVector() / JAGGED_RESOLUTION;
-		double sep = SEPARATION / 2;
+			this.points1.add(point);
 
-		points1.add(point1);
-		for (int i = 0; i < SEGMENT_COUNT; i++) {
-			double x = i / SEGMENT_COUNT;
-			double maximumHeight = (1 - Math.abs(0.5 - x) * 2) * HEIGHT;
-			points1.add(diff
-					.scale(x + rand.nextDouble(-sep, sep))
-					.addVector(0, rand.nextDouble(-JAGGED_RESOLUTION, JAGGED_RESOLUTION) + maximumHeight, 0));
+			Vec3d sub = secPoint.subtract(point);
+
+			for (int j = 0; j < SEGMENT_COUNT; j++) {
+				double x = j / SEGMENT_COUNT;
+				Vec3d scale = sub.scale(x).add(point).addVector(
+						rand.nextDouble(-JAGGED_RESOLUTION, JAGGED_RESOLUTION),
+						rand.nextDouble(-JAGGED_RESOLUTION, JAGGED_RESOLUTION),
+						0);
+				this.points1.add(scale);
+			}
 		}
-		points1.add(point2);
+		this.points1.add(points1.get(points1.size() - 1));
 
-		points2.add(point1);
-		for (int i = 0; i < SEGMENT_COUNT; i++) {
-			double x = i / SEGMENT_COUNT;
-			double maximumHeight = (1 - Math.abs(0.5 - x) * 2) * HEIGHT;
-			points2.add(diff
-					.scale(x + rand.nextDouble(-sep, sep))
-					.addVector(0, rand.nextDouble(-JAGGED_RESOLUTION, JAGGED_RESOLUTION) - maximumHeight, 0));
+		for (int i = 0; i < points2.size() - 1; i++) {
+			Vec3d point = points2.get(i);
+			Vec3d secPoint = points2.get(i + 1);
+
+			this.points2.add(point);
+
+			Vec3d sub = secPoint.subtract(point);
+
+			for (int j = 0; j < SEGMENT_COUNT; j++) {
+				double x = j / SEGMENT_COUNT;
+				Vec3d scale = sub.scale(x).add(point).addVector(
+						rand.nextDouble(-JAGGED_RESOLUTION, JAGGED_RESOLUTION),
+						rand.nextDouble(-JAGGED_RESOLUTION, JAGGED_RESOLUTION),
+						0);
+				this.points2.add(scale);
+			}
 		}
-		points2.add(point2);
+		this.points2.add(points2.get(points2.size() - 1));
+
+		for (Vec3d point : this.points1) {
+			Vec3d newpoint = new Vec3d(point.x + rand.nextDouble(-1, 1), point.y + rand.nextDouble(-1, 1), point.z + rand.nextDouble(-1, 1));
+			points3.add(newpoint);
+		}
+
+		for (Vec3d point : this.points2) {
+			Vec3d newpoint = new Vec3d(point.x + rand.nextDouble(-1, 1), point.y + rand.nextDouble(-1, 1), point.z + rand.nextDouble(-1, 1));
+			points4.add(newpoint);
+		}
+
+		// NORMALS
+
+		for (int i = 0; i < this.points1.size() - 1; i++) {
+			Vec3d point1 = this.points1.get(i);
+			Vec3d point2 = this.points1.get(i + 1);
+			Vec3d sub = point1.subtract(point2);
+			Vec3d cross = sub.crossProduct(new Vec3d(0, 0, 1)).normalize();
+
+			normalPoints1.add(cross);
+		}
+		for (int i = 0; i < points3.size() - 1; i++) {
+			Vec3d point1 = points3.get(i);
+			Vec3d point2 = points3.get(i + 1);
+			Vec3d sub = point1.subtract(point2);
+			Vec3d cross = sub.crossProduct(new Vec3d(0, 0, 1)).normalize();
+
+			normalPoints3.add(cross);
+		}
+		for (int i = 0; i < this.points2.size() - 1; i++) {
+			Vec3d point1 = this.points2.get(i);
+			Vec3d point2 = this.points2.get(i + 1);
+			Vec3d sub = point1.subtract(point2);
+			Vec3d cross = sub.crossProduct(new Vec3d(0, 0, -1)).normalize();
+
+			normalPoints2.add(cross);
+		}
+		for (int i = 0; i < points4.size() - 1; i++) {
+			Vec3d point1 = points4.get(i);
+			Vec3d point2 = points4.get(i + 1);
+			Vec3d sub = point1.subtract(point2);
+			Vec3d cross = sub.crossProduct(new Vec3d(0, 0, -1)).normalize();
+
+			normalPoints4.add(cross);
+		}
 	}
 
 	private static VertexBuffer pos(VertexBuffer vb, Vec3d pos) {
@@ -99,55 +165,80 @@ public class RiftRenderer {
 		double interpPosZ = player.lastTickPosZ + (player.posZ - player.lastTickPosZ) * event.getPartialTicks();
 
 		GlStateManager.translate(-interpPosX, -interpPosY, -interpPosZ);
-		//GlStateManager.translate(-interpPosX, -interpPosY, -interpPosZ);
 
-		GlStateManager.depthMask(false);
 		GlStateManager.disableCull();
 		GlStateManager.enableAlpha();
 		GlStateManager.enableBlend();
 
+		Color color = new Color(0x965aff48, true);
+		Color gradColor = new Color(0xe80bc6);
+
+		GlStateManager.rotate(Minecraft.getMinecraft().player.rotationYaw, 0, -1, 0);
+
 		Tessellator tessellator = Tessellator.getInstance();
 		VertexBuffer vb = tessellator.getBuffer();
-		vb.begin(GL11.GL_TRIANGLE_FAN, DefaultVertexFormats.POSITION_COLOR);
-		for (int i = 0; i < points1.size(); i += (flip ? 1 : 0)) {
+
+		// RIFT
+
+		GlStateManager.depthMask(true);
+		GL14.glBlendEquation(GL_FUNC_REVERSE_SUBTRACT);
+
+		vb.begin(GL11.GL_TRIANGLE_STRIP, DefaultVertexFormats.POSITION_COLOR);
+		for (int i = 0; i < points1.size(); i += (flip1 ? 1 : 0)) {
 			Vec3d point;
-			if (flip) point = points1.get(i);
-			else point = points2.get(i);
+			if (flip1) point = points1.get(i);
+			else point = points3.get(i);
 
-			flip = !flip;
+			flip1 = !flip1;
 
-			Color color = new Color(RandUtil.nextFloat(), RandUtil.nextFloat(), RandUtil.nextFloat());
+			point = point.add(
+					new Vec3d(
+							MathHelper.sin((float) (((ClientTickHandler.getTicks() / 30.0) + point.lengthVector()))),
+							MathHelper.sin((float) (((ClientTickHandler.getTicks() / 30.0) + point.lengthVector() * 0.5))),
+							MathHelper.sin((float) (((ClientTickHandler.getTicks() / 30.0) + point.lengthVector() * 1.5)))).scale(0.3));
 
 			pos(vb, point).color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).endVertex();
 		}
 		tessellator.draw();
 
-		//GL11.glEnable(GL11.GL_STENCIL_TEST);
-		//GL11.glColorMask(false, false, false, false);
-		//GL11.glStencilFunc(GL_ALWAYS, 0, 1);
-		//GL11.glStencilOp(GL_KEEP, GL_KEEP, GL_INVERT);
-		//GL11.glStencilMask(1);
-		////GlStateManager.blendFunc(GL_SRC_ALPHA, GL_ONE);
-//
-		//Tessellator tessellator = Tessellator.getInstance();
-		//VertexBuffer vb = tessellator.getBuffer();
-		//vb.begin(GL11.GL_TRIANGLE_FAN, DefaultVertexFormats.POSITION_COLOR);
-//
-		//for (int i = 0; i < points.size() - 1; i++) {
-		//	Vec3d current = points.get(i).add(center);
-		//	Color color = new Color(RandUtil.nextFloat(), RandUtil.nextFloat(), RandUtil.nextFloat());
-//
-		//	pos(vb, current).color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).endVertex();
-		//}
-//
-		//GL11.glColorMask(true, true, true, true);
-		//GL11.glStencilFunc(GL_EQUAL, 1, 1);
-		//GL11.glStencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
-//
-//
-		//GL11.glDisable(GL_STENCIL_TEST);
+		vb.begin(GL11.GL_TRIANGLE_STRIP, DefaultVertexFormats.POSITION_COLOR);
+		for (int i = 0; i < points2.size(); i += (flip2 ? 1 : 0)) {
+			Vec3d point;
+			if (flip2) point = points2.get(i);
+			else point = points4.get(i);
 
-		GlStateManager.depthMask(true);
+			flip2 = !flip2;
+
+			point = point.add(
+					new Vec3d(
+							MathHelper.sin((float) (((ClientTickHandler.getTicks() / 30.0) + point.lengthVector()))),
+							MathHelper.sin((float) (((ClientTickHandler.getTicks() / 30.0) + point.lengthVector() * 0.5))),
+							MathHelper.sin((float) (((ClientTickHandler.getTicks() / 30.0) + point.lengthVector() * 1.5)))).scale(0.3));
+
+			pos(vb, point).color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).endVertex();
+		}
+		tessellator.draw();
+
+		vb.begin(GL11.GL_TRIANGLE_STRIP, DefaultVertexFormats.POSITION_COLOR);
+		for (int i = 0; i < points3.size(); i += (flip3 ? 1 : 0)) {
+			Vec3d point;
+			if (flip3) point = points3.get(i);
+			else point = points4.get(i);
+
+			flip3 = !flip3;
+
+			point = point.add(
+					new Vec3d(
+							MathHelper.sin((float) (((ClientTickHandler.getTicks() / 30.0) + point.lengthVector()))),
+							MathHelper.sin((float) (((ClientTickHandler.getTicks() / 30.0) + point.lengthVector() * 0.5))),
+							MathHelper.sin((float) (((ClientTickHandler.getTicks() / 30.0) + point.lengthVector() * 1.5)))).scale(0.3));
+
+			pos(vb, point).color(color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha()).endVertex();
+		}
+		tessellator.draw();
+
+		GL14.glBlendEquation(GL_FUNC_ADD);
+
 		GlStateManager.popMatrix();
 	}
 }
