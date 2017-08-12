726be4bf83eed66115417ee31b5b7590e7e5f7e1
yrsegal <yrsegal@gmail.com>
2017-08-12 19:42:11 -0400

    most of projectile framework

---
+++ src/main/java/eladkay/quaeritum/common/entity/EntityBaseProjectile.kt
@@ -0,0 +1,253 @@
+package eladkay.quaeritum.common.entity
+
+import com.google.common.base.Predicate
+import com.google.common.base.Predicates
+import net.minecraft.entity.Entity
+import net.minecraft.entity.EntityLivingBase
+import net.minecraft.entity.IProjectile
+import net.minecraft.entity.monster.EntityEnderman
+import net.minecraft.entity.player.EntityPlayer
+import net.minecraft.init.SoundEvents
+import net.minecraft.nbt.NBTTagCompound
+import net.minecraft.util.DamageSource
+import net.minecraft.util.EntitySelectors
+import net.minecraft.util.EnumParticleTypes
+import net.minecraft.util.math.MathHelper
+import net.minecraft.util.math.RayTraceResult
+import net.minecraft.util.math.Vec3d
+import net.minecraft.world.World
+import net.minecraftforge.common.util.Constants
+
+
+/**
+ * @author WireSegal
+ * Created at 6:32 PM on 8/7/17.
+ */
+@Suppress("LeakingThis")
+abstract class EntityBaseProjectile(worldIn: World) : Entity(worldIn), IProjectile {
+    var shootingEntity: Entity? = null
+    var damage = 0.0
+    var knockback = 0.0
+
+    init {
+        damage = 2.0
+        setSize(0.5f, 0.5f)
+    }
+
+    constructor(worldIn: World, x: Double, y: Double, z: Double) : this(worldIn) {
+        setPosition(x, y, z)
+    }
+
+    constructor(worldIn: World, shooter: EntityLivingBase) : this(worldIn, shooter.posX, shooter.posY + shooter.eyeHeight.toDouble() - 0.10000000149011612, shooter.posZ) {
+        shootingEntity = shooter
+    }
+
+    fun setAim(shooter: Entity, pitch: Float, yaw: Float, velocity: Float, inaccuracy: Float) {
+        val xHeading = -MathHelper.sin(yaw * Math.PI.toFloat() / 180) * MathHelper.cos(pitch * Math.PI.toFloat() / 180)
+        val yHeading = -MathHelper.sin(pitch * Math.PI.toFloat() / 180)
+        val zHeading = MathHelper.cos(yaw * Math.PI.toFloat() / 180) * MathHelper.cos(pitch * Math.PI.toFloat() / 180)
+        setThrowableHeading(xHeading.toDouble(), yHeading.toDouble(), zHeading.toDouble(), velocity, inaccuracy)
+        motionX += shooter.motionX
+        motionZ += shooter.motionZ
+
+        if (!shooter.onGround) motionY += shooter.motionY
+    }
+
+    override fun setThrowableHeading(xOrig: Double, yOrig: Double, zOrig: Double, velocity: Float, inaccuracy: Float) {
+        var x = xOrig
+        var y = yOrig
+        var z = zOrig
+        val magnitude = MathHelper.sqrt(x * x + y * y + z * z)
+        x /= magnitude.toDouble()
+        y /= magnitude.toDouble()
+        z /= magnitude.toDouble()
+        x += rand.nextGaussian() * 0.0075 * inaccuracy.toDouble()
+        y += rand.nextGaussian() * 0.0075 * inaccuracy.toDouble()
+        z += rand.nextGaussian() * 0.0075 * inaccuracy.toDouble()
+        x *= velocity.toDouble()
+        y *= velocity.toDouble()
+        z *= velocity.toDouble()
+        motionX = x
+        motionY = y
+        motionZ = z
+        val horizontalMagnitude = MathHelper.sqrt(x * x + z * z)
+        rotationYaw = (MathHelper.atan2(x, z) * (180.0 / Math.PI)).toFloat()
+        rotationPitch = (MathHelper.atan2(y, horizontalMagnitude.toDouble()) * (180.0 / Math.PI)).toFloat()
+        prevRotationYaw = rotationYaw
+        prevRotationPitch = rotationPitch
+    }
+
+    override fun setVelocity(x: Double, y: Double, z: Double) {
+        super.setVelocity(x, y, z)
+
+        if (prevRotationPitch == 0.0f && prevRotationYaw == 0.0f) {
+            val f = MathHelper.sqrt(x * x + z * z)
+            rotationPitch = (MathHelper.atan2(y, f.toDouble()) * (180.0 / Math.PI)).toFloat()
+            rotationYaw = (MathHelper.atan2(x, z) * (180.0 / Math.PI)).toFloat()
+            prevRotationPitch = rotationPitch
+            prevRotationYaw = rotationYaw
+            setLocationAndAngles(posX, posY, posZ, rotationYaw, rotationPitch)
+        }
+    }
+
+    override fun onUpdate() {
+        super.onUpdate()
+
+        if (prevRotationPitch == 0.0f && prevRotationYaw == 0.0f) {
+            val f = MathHelper.sqrt(motionX * motionX + motionZ * motionZ)
+            rotationYaw = (MathHelper.atan2(motionX, motionZ) * (180.0 / Math.PI)).toFloat()
+            rotationPitch = (MathHelper.atan2(motionY, f.toDouble()) * (180.0 / Math.PI)).toFloat()
+            prevRotationYaw = rotationYaw
+            prevRotationPitch = rotationPitch
+        }
+
+        val position = Vec3d(posX, posY, posZ)
+        var projected = Vec3d(posX + motionX, posY + motionY, posZ + motionZ)
+        var trace = world.rayTraceBlocks(position, projected, false, true, false)
+
+        if (trace != null)
+            projected = Vec3d(trace.hitVec.x, trace.hitVec.y, trace.hitVec.z)
+
+        val entity = findEntityOnPath(position, projected)
+
+        if (entity != null)
+            trace = RayTraceResult(entity)
+
+        if (trace != null && trace.entityHit is EntityPlayer && shootingEntity is EntityPlayer &&
+                (shootingEntity as EntityPlayer).canAttackPlayer(trace.entityHit as EntityPlayer))
+                trace = null
+
+        if (trace != null)
+            onHit(trace)
+
+        posX += motionX
+        posY += motionY
+        posZ += motionZ
+        val horizontalMagnitude = MathHelper.sqrt(motionX * motionX + motionZ * motionZ)
+        rotationYaw = (MathHelper.atan2(motionX, motionZ) * (180.0 / Math.PI)).toFloat()
+
+        rotationPitch = (MathHelper.atan2(motionY, horizontalMagnitude.toDouble()) * (180.0 / Math.PI)).toFloat()
+        while (rotationPitch - prevRotationPitch < -180.0f) prevRotationPitch -= 360.0f
+        while (rotationPitch - prevRotationPitch >= 180.0f) prevRotationPitch += 360.0f
+        while (rotationYaw - prevRotationYaw < -180.0f) prevRotationYaw -= 360.0f
+        while (rotationYaw - prevRotationYaw >= 180.0f) prevRotationYaw += 360.0f
+
+        rotationPitch = prevRotationPitch + (rotationPitch - prevRotationPitch) * 0.2f
+        rotationYaw = prevRotationYaw + (rotationYaw - prevRotationYaw) * 0.2f
+        var motionModifier = 0.99f
+
+        if (isInWater) {
+            for (i in 0..3)
+                world.spawnParticle(EnumParticleTypes.WATER_BUBBLE, posX - motionX * 0.25, posY - motionY * 0.25, posZ - motionZ * 0.25, motionX, motionY, motionZ, *IntArray(0))
+            motionModifier = 0.6f
+        }
+
+        if (isWet) extinguish()
+
+        motionX *= motionModifier.toDouble()
+        motionY *= motionModifier.toDouble()
+        motionZ *= motionModifier.toDouble()
+
+        if (!hasNoGravity()) motionY -= 0.05
+
+        setPosition(posX, posY, posZ)
+        doBlockCollisions()
+    }
+
+    abstract fun getDefaultDamageSource(): DamageSource
+    abstract fun getShotDamageSource(shooter: Entity): DamageSource
+    abstract fun onImpact(trace: RayTraceResult): DamageSource
+
+    protected fun onHit(trace: RayTraceResult) {
+        onImpact(trace)
+        val entity = trace.entityHit
+
+        if (entity != null) {
+            val magnitude = MathHelper.sqrt(motionX * motionX + motionY * motionY + motionZ * motionZ)
+            val damage = MathHelper.ceil(magnitude.toDouble() * damage)
+
+            val shooter = shootingEntity
+            val source = if (shooter != null)
+                getShotDamageSource(shooter)
+            else
+                getDefaultDamageSource()
+
+            if (isBurning && entity !is EntityEnderman)
+                entity.setFire(5)
+
+            if (entity.attackEntityFrom(source, damage.toFloat())) {
+                if (entity is EntityLivingBase) {
+
+                    if (knockback > 0) {
+                        val f1 = MathHelper.sqrt(motionX * motionX + motionZ * motionZ)
+                        if (f1 > 0.0f)
+                            entity.addVelocity(motionX * knockback * 0.6 / f1.toDouble(), 0.1, motionZ * knockback * 0.6 / f1.toDouble())
+                    }
+
+                    arrowHit(entity)
+                }
+
+                playSound(SoundEvents.ENTITY_ARROW_HIT, 1.0f, 1.2f / (rand.nextFloat() * 0.2f + 0.9f))
+
+                if (entity !is EntityEnderman)
+                    setDead()
+            } else {
+                motionX *= -0.1
+                motionY *= -0.1
+                motionZ *= -0.1
+                rotationYaw += 180.0f
+                prevRotationYaw += 180.0f
+            }
+        } else
+            setDead()
+
+    }
+
+    protected open fun arrowHit(living: EntityLivingBase) {}
+
+    protected fun findEntityOnPath(start: Vec3d, end: Vec3d): Entity? {
+        var entity: Entity? = null
+        val list = world.getEntitiesInAABBexcluding(this, entityBoundingBox.expand(motionX, motionY, motionZ).grow(1.0), ARROW_TARGETS)
+        var maxDistance = 0.0
+
+        for (i in list.indices) {
+            val entity1 = list[i]
+
+            if (entity1 !== shootingEntity) {
+                val boundingBox = entity1.entityBoundingBox.grow(0.3)
+                val trace = boundingBox.calculateIntercept(start, end)
+
+                if (trace != null) {
+                    val d1 = start.squareDistanceTo(trace.hitVec)
+
+                    if (d1 < maxDistance || maxDistance == 0.0) {
+                        entity = entity1
+                        maxDistance = d1
+                    }
+                }
+            }
+        }
+
+        return entity
+    }
+
+    public override fun writeEntityToNBT(compound: NBTTagCompound) {
+        compound.setDouble("damage", damage)
+        compound.setDouble("knockback", knockback)
+    }
+
+    public override fun readEntityFromNBT(compound: NBTTagCompound) {
+        if (compound.hasKey("damage", Constants.NBT.TAG_ANY_NUMERIC)) damage = compound.getDouble("damage")
+        if (compound.hasKey("knockback", Constants.NBT.TAG_ANY_NUMERIC)) damage = compound.getDouble("knockback")
+    }
+
+    override fun canTriggerWalking() = false
+
+    override fun canBeAttackedWithItem() = false
+
+    override fun getEyeHeight() = 0.0f
+
+    companion object {
+        private val ARROW_TARGETS = Predicates.and(EntitySelectors.NOT_SPECTATING, EntitySelectors.IS_ALIVE, Predicate<Entity> { it!!.canBeCollidedWith() })
+    }
+}
