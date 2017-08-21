d649d873bbeb4b1ab8512efe20e51af158cbcced
yrsegal <yrsegal@gmail.com>
2017-08-21 18:52:32 -0400

    alchemical compositions

---
+++ src/main/java/eladkay/quaeritum/api/alchemy/AlchemicalCompositions.java
@@ -0,0 +1,51 @@
+package eladkay.quaeritum.api.alchemy;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidStack;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author WireSegal
+ *         Created at 9:56 AM on 2/1/17.
+ */
+public final class AlchemicalCompositions {
+    @NotNull
+    private static final List<IAlchemicalComposition> recipes = new ArrayList<>();
+
+    @NotNull
+    public static List<IAlchemicalComposition> getRecipes() {
+        return recipes;
+    }
+
+    @Nullable
+    public static IAlchemicalComposition getRecipe(@NotNull FluidStack stack, @NotNull ItemStack dust) {
+        for (IAlchemicalComposition recipe : recipes)
+            if (recipe.isCompositable(stack, dust))
+                return recipe;
+        return null;
+    }
+
+    @Nullable
+    public static IAlchemicalComposition getRecipe(@NotNull ItemStack composite) {
+        for (IAlchemicalComposition recipe : recipes)
+            if (recipe.isDecomposable(composite))
+                return recipe;
+        return null;
+    }
+
+    @NotNull
+    public static IAlchemicalComposition registerRecipe(@NotNull IAlchemicalComposition recipe) {
+        recipes.add(recipe);
+        return recipe;
+    }
+
+    @NotNull
+    public static IAlchemicalComposition registerRecipe(@NotNull Fluid liquid, @NotNull ItemStack dust, @NotNull ItemStack composite) {
+        return registerRecipe(new BaseAlchemicalComposition(liquid, dust, composite));
+    }
+}
---
+++ src/main/java/eladkay/quaeritum/api/alchemy/IAlchemicalComposition.java
@@ -0,0 +1,76 @@
+package eladkay.quaeritum.api.alchemy;
+
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fluids.FluidStack;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * @author WireSegal
+ *         Created at 7:59 PM on 8/20/17.
+ */
+public interface IAlchemicalComposition {
+
+    /**
+     * All fluid stacks involved in composition should take and produce 500 mB.
+     */
+    int LIQUID_VOLUME = 500;
+
+
+    /**
+     * Gets the liquid stack that the reagent will decompose to.
+     * @param composite The composite stack, in case data needs to be preserved.
+     * @return The liquid stack produced by decomposing this composite.
+     */
+    @NotNull
+    FluidStack getLiquidStack(@NotNull ItemStack composite);
+
+    /**
+     * Gets the solid dust stack that the reagent will decompose to.
+     * @param composite The composite stack, in case data needs to be preserved.
+     * @return The residue stack produced by decomposing this composite.
+     */
+    @NotNull
+    ItemStack getDustStack(@NotNull ItemStack composite);
+
+    /**
+     * Gets the resulting composite stack that the fluid and dust will compose to.
+     * @param fluid The fluid stack, in case data needs to be preserved.
+     * @param dust The dust stack, in case data needs to be preserved.
+     * @return The composite of the fluid and the dust.
+     */
+    @NotNull
+    ItemStack getCompositeStack(@NotNull FluidStack fluid, @NotNull ItemStack dust);
+
+    /**
+     * Whether a fluid and a dust can be composited with this recipe.
+     * @param fluid The fluid to be checked.
+     * @param dust The dust to be checked.
+     * @return Whether the composite is valid.
+     */
+    boolean isCompositable(@NotNull FluidStack fluid, @NotNull ItemStack dust);
+
+    /**
+     * Whether a composite can be decomposed into a liquid and a dust.
+     * @param composite The composite to be checked.
+     * @return Whether the decomposition is valid.
+     */
+    boolean isDecomposable(@NotNull ItemStack composite);
+
+    /**
+     * @return An example liquid, for JEI and similar displays.
+     */
+    @NotNull
+    FluidStack getExampleLiquidStack();
+
+    /**
+     * @return An example dust, for JEI and similar displays.
+     */
+    @NotNull
+    ItemStack getExampleDustStack();
+
+    /**
+     * @return An example composite, for JEI and similar displays.
+     */
+    @NotNull
+    ItemStack getExampleCompositeStack();
+}
