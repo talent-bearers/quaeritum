897d16b8768149727f11cbf44dd05d0a20a698b0
yrsegal <yrsegal@gmail.com>
2018-02-26 16:27:46 -0500

    fix a lighting issue

--- src/main/java/eladkay/quaeritum/client/gui/book/ComponentRecipe.java
+++ src/main/java/eladkay/quaeritum/client/gui/book/ComponentRecipe.java
@@ -7,6 +7,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.recipebook.GhostRecipe;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.inventory.Slot;
@@ -134,6 +135,7 @@ public class ComponentRecipe extends GuiComponent {
             }
 
             GlStateManager.popMatrix();
+            RenderHelper.enableStandardItemLighting();
         });
     }
 
