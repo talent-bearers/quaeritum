07de41842be857f633e64306e052904ba97bac74
InsomniaKitten <admin@designdevils.co.uk>
2018-04-10 16:30:36 +0100

    Fix centrifuge facing incorrect direction when placed

--- src/main/java/eladkay/quaeritum/common/block/machine/BlockCentrifuge.kt
+++ src/main/java/eladkay/quaeritum/common/block/machine/BlockCentrifuge.kt
@@ -47,7 +47,7 @@ class BlockCentrifuge : BlockModContainer(LibNames.CENTRIFUGE, Material.CLOTH) {
 
     override fun getMetaFromState(state: IBlockState) = state.getValue(FACING).horizontalIndex
 
-    override fun getStateForPlacement(world: World?, pos: BlockPos?, facing: EnumFacing?, hitX: Float, hitY: Float, hitZ: Float, meta: Int, placer: EntityLivingBase, hand: EnumHand?): IBlockState = defaultState.withProperty(BlockHorizontal.FACING, placer.horizontalFacing)
+    override fun getStateForPlacement(world: World?, pos: BlockPos?, facing: EnumFacing?, hitX: Float, hitY: Float, hitZ: Float, meta: Int, placer: EntityLivingBase, hand: EnumHand?): IBlockState = defaultState.withProperty(BlockHorizontal.FACING, placer.horizontalFacing.opposite)
 
     override fun hasComparatorInputOverride(state: IBlockState): Boolean = true
     override fun getComparatorInputOverride(blockState: IBlockState, worldIn: World, pos: BlockPos): Int {
